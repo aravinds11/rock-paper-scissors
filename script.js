309113d4ffec086ae412ab5c71134881a6ba6ec2
aravinds11 <174857986+aravinds11@users.noreply.github.com>
2024-12-07 21:14:16 +0530

    Add function to get user input

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 let computerChoice;
+let humanChoice;
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 99) + 1;
     if(randomNumber >= 0 && randomNumber <= 33) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
     }
     else if(randomNumber >= 34 && randomNumber <= 66) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     }
     else {
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
     }
     console.log(computerChoice);
 }
 
+function getHumanChoice() {
+    humanChoice = prompt("Enter your choice: ");
+    humanChoice = humanChoice.toLowerCase();
+    alert(humanChoice);
+}
+
 getComputerChoice();
+getHumanChoice();
